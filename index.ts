c3317d3c4d872024a3b6a9a129aa5a4403fe7062
Danny <223181@ids.upchiapas.edu.mx>
2024-03-01 23:04:15 -0600

    Creacion del servidor-ws

---
+++ index.ts
@@ -0,0 +1,37 @@
+import cors from "cors";
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { EventsSocket } from "./src/entities/eventTypes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.set("port", process.env.PORT || 4000);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to my api");
+});
+
+app.use("*", (req, res) => {
+  res.send("Esta ruta no existe en la API");
+});
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  },
+});
+io.on(EventsSocket.CONNECTION, (socket) => {
+  socket.on(EventsSocket.SEND_DATA, (data: any) => {
+    console.log(data);
+    io.emit(EventsSocket.RECEIVE_DATA, data);
+  });
+});
+
+server.listen(app.get("port"), () => {
+  console.log("Servidor corriendo en puerto", app.get("port"));
+});
